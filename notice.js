96e6da9623ac1c268da55d8f95737ac81748198e
Simon <simon.kelch@outlook.de>
2022-03-17 17:43:14 +0100

    notice banner & ribbon

---
+++ notice.js
@@ -0,0 +1,20 @@
+const noticeClose = document.querySelector(".noticeclose");
+const noticebanner = document.querySelector(".noticebanner");
+const noticetoggle = document.querySelector(".mail-icon");
+
+noticeClose.onclick = function () {
+  noticebanner.classList.toggle('showNotice');
+  localStorage.setItem("noticeStatus", 1);
+}
+noticetoggle.onclick = function () {
+    noticebanner.classList.toggle('showNotice');
+    localStorage.setItem("noticeStatus", 0);
+  }
+
+/*-- Theme Notice Anfang */
+const notice = document.querySelector(".notice-toggle");
+const currentNotice = localStorage.getItem("noticeStatus");
+if (currentNotice == "1") {
+} else if (currentNotice == "0") {
+noticebanner.classList.add('showNotice');
+}
